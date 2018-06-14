6fb8e877efb52fca200d18082a8e9e9502757227
huinalam <huinalam@gmail.com>
2018-06-14 12:27:17 +0000

    cut lower part in picture. if black screen, dont upload

--- take_picture.py
+++ take_picture.py
@@ -22,11 +22,18 @@ def get_reversed_unix_time():
     time_data = time.time()
     return str(int(max_value-time_data))
 
+def is_night(df):
+    if (np.mean(df.red) < 10.0 and
+        np.mean(df.blue) < 10.0 and
+        np.mean(df.green) < 10.0):
+        return True
+    return False
+
 def upload_pciture():
     stream = io.BytesIO()
     with picamera.PiCamera() as picam:
         picam.start_preview()
-        picam.vflip = True
+        picam.vflip = False
         time.sleep(2)
         picam.capture(stream, format='bmp')
         picam.stop_preview()
@@ -37,33 +44,61 @@ def upload_pciture():
     # resize
     image = image.resize((int(720/8), int(480/8)), Image.ANTIALIAS)
     
+    # 아래 45 픽셀 삭제
+    im_data = np.asarray(image)
+    crop_image = im_data[:45]
+    im = Image.fromarray(crop_image)
+    image = im
+    
+    # 데이터를 하나의 배열로 정리
+    image_dataset = []
+    for row in im_data:
+        for item in row:
+            image_dataset.append(item)
+    image_dataset = np.array(image_dataset)
+    df = pd.DataFrame(image_dataset, columns=['red','green','blue'])
+    
     with open('/home/pi/projects/sunset-list/api_key.json', 'r') as f:
         app_key = json.loads(f.read())
-        # stream image upload
-        imagefile = io.BytesIO()
-        image.save(imagefile, format='BMP')
-        imagefile.seek(0)
-        
-        file_name = '{0}.bmp'.format(get_reversed_unix_time())
-        block_blob_service = BlockBlobService(connection_string=app_key['azure_storage_connection'])
-        blob = block_blob_service.create_blob_from_stream('nuknuk', file_name, imagefile)
-        imagefile.close()
-        
-        file_url = 'https://urbanlist.blob.core.windows.net/nuknuk/{0}'.format(file_name)
-        
         table_service = TableService(connection_string=app_key['azure_storage_connection'])
-        task = {
-            'PartitionKey': 'nyuknyuk',
-            'RowKey': '{0}'.format(get_reversed_unix_time()), 
-            'Green' : 0,
-            'Red' : 0,
-            'Blue': 0,
-            'FilePath':file_url}
-        table_service.insert_entity('nuknuk', task)
+        
+        if is_night(df):
+            task = {
+                'PartitionKey': 'nyuknyuk',
+                'RowKey': '{0}'.format(get_reversed_unix_time()), 
+                'Green' : 0,
+                'Red' : 0,
+                'Blue': 0,
+                'FilePath':None}
+            table_service.insert_entity('nuknuk', task)
+        else:
+            # stream image upload
+            imagefile = io.BytesIO()
+            image.save(imagefile, format='BMP')
+            imagefile.seek(0)
+
+            file_name = '{0}.bmp'.format(get_reversed_unix_time())
+            block_blob_service = BlockBlobService(connection_string=app_key['azure_storage_connection'])
+            blob = block_blob_service.create_blob_from_stream('nuknuk', file_name, imagefile)
+            imagefile.close()
+
+            file_url = 'https://urbanlist.blob.core.windows.net/nuknuk/{0}'.format(file_name)
+
+            task = {
+                'PartitionKey': 'nyuknyuk',
+                'RowKey': '{0}'.format(get_reversed_unix_time()), 
+                'Green' : 0,
+                'Red' : 0,
+                'Blue': 0,
+                'FilePath':file_url}
+            table_service.insert_entity('nuknuk', task)
     
     stream.close()
 
 if __name__ == '__main__':
-    upload_pciture()
     with open('/home/pi/projects/sunset-list/log.log', 'a') as f:
-        f.write(str(datetime.datetime.now())+'\n')
+        f.write('run: '+str(datetime.datetime.now())+'\n')
+        try:
+            upload_pciture()
+        except Exception as e:
+            f.write('except: '+str(e)+'\n')
